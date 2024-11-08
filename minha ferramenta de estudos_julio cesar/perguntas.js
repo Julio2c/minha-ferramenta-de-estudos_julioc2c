ea296b636259722d539cbe1504681ab2b186bf53
Julio2c <00001123497242sp@al.educacao.sp.gov.br>
2024-11-08 11:33:35 -0300

    Add files via upload

---
+++ minha ferramenta de estudos_julio cesar/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é standoff 2?',
+    'O standoff 2 é um jogo de tiro de estrategia de dois lados, um que ataca e um que defende'
+)
+
+criaCartao(
+    'matemarica',
+    'qual a diferença entre ponto maximo e o ponto minimo na equação de 2 grau?',
+    'A diferença entre os dois é o A ou p ´´x²´´ se ele estiver negativo ponto maximo ou se estiver positivo é o ponto minimo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz vermelho em Inglês?',
+    'vermelho em ingles é red (Red)'
+)
